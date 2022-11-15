5e12519ea3d56e3979fc91b00a4081f5f23512fb
Priyank-Patel-007 <priyank4870@gmail.com>
2022-11-15 18:13:15 -0500

    Initial Commit

---
+++ project02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project02
+//
+//  Created by Priyank Patel on 2022-11-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
